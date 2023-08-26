879b84e5b6fff9be3ec52997a0986463e017d01d
Kushal Das <kushaldas30102002@gmail.com>
2023-08-26 22:17:33 +0530

    updated About.jsx timeLineData

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,11 @@
-import { AbsoluteCenter, Box, Center, Flex, Heading, Stack, Text } from '@chakra-ui/react'
+import { AbsoluteCenter,Box,Center,Flex,Heading,Stack,Text } from '@chakra-ui/react'
 import { Divider } from '@chakra-ui/react'
 import { motion } from 'framer-motion'
 import { ScrollText } from '../../utils/ScrollText';
 
 import EducationTimeline from './EducationTimeline'
 
-import timelineData from '../../data/timelineData'
+import timelineData from '../../data/timelineData.js'
 
 export default function About() {
 
@@ -18,48 +18,48 @@ export default function About() {
   ];
 
   return (
-    <Flex id='about' flexDirection={['column', 'row']} justifyContent={'center'} alignItems={'center'} h={'100vh'} w='100vw' bgGradient="linear(to-b,black,gray.900)">
+    <Flex id='about' flexDirection={['column','row']} justifyContent={'center'} alignItems={'center'} h={'100vh'} w='100vw' bgGradient="linear(to-b,black,gray.900)">
 
       <Flex
         // bg="blue.500"
         h={['50vh','100vh']}
-        w={['97vw', '50vw']}
-        pt={[0, 10]}
+        w={['97vw','50vw']}
+        pt={[0,10]}
         justifyContent={'normal'}
         alignItems={'center'}
         flexDirection={'column'}
         color={'white'}
-        // ml={'2vw'}
-        // bgColor={'#000'}
+      // ml={'2vw'}
+      // bgColor={'#000'}
 
       >
         <motion.div
-          initial={{ opacity: 0, x: -100 }}
-          whileInView={{ x: 0, opacity: 1, transition: { duration: .5, delay: .2 } }}
+          initial={{ opacity: 0,x: -100 }}
+          whileInView={{ x: 0,opacity: 1,transition: { duration: .5,delay: .2 } }}
         // viewport={{once:true}}
         // onViewportLeave={{ opacity: 0, x: -100 ,transition: { duration: .5,delay:.5 } }}
         >
           <Flex justifyContent={'center'} alignItems={'center'} w={'100vw'}>
 
-            <Text color={'red'} fontSize={['4xl', '6xl']} fontWeight={'light'}>
+            <Text color={'red'} fontSize={['4xl','6xl']} fontWeight={'light'}>
               About  </Text>
-            <Text color='red' fontSize={['4xl', '6xl']} fontWeight={'light'}>.me</Text>
+            <Text color='red' fontSize={['4xl','6xl']} fontWeight={'light'}>.me</Text>
           </Flex>
 
         </motion.div>
         {/* here goes the content */}
         <motion.div
-          initial={{ opacity: 0, x: 100 }}
-          whileInView={{ x: 0, opacity: 1, transition: { duration: .5, delay: .4 } }}
-          style={{ overflow: 'scroll', scrollBehavior: 'smooth' }}
+          initial={{ opacity: 0,x: 100 }}
+          whileInView={{ x: 0,opacity: 1,transition: { duration: .5,delay: .4 } }}
+          style={{ overflow: 'scroll',scrollBehavior: 'smooth' }}
         // viewport={{once:true}}
         // onViewportLeave={{ opacity: 0, x: -100 ,transition: { duration: .5,delay:.5 } }}
         >
           <Text
             fontSize={['sm','3xl']}
-            px={[0, 20]}
-            textAlign={['center', 'left']}
-            pt={[0, 10]}
+            px={[0,20]}
+            textAlign={['center','left']}
+            pt={[0,10]}
           >
 
             <ScrollText content={aboutMe} />
@@ -67,11 +67,11 @@ export default function About() {
         </motion.div>
       </Flex>
 
-      
-       
-        
 
-        
+
+
+
+
 
 
 
@@ -79,33 +79,33 @@ export default function About() {
         // bg="#000"
         h={'100vh'}
         color='#fff'
-        w={['100vw', '57vw']}
+        w={['100vw','57vw']}
         justifyContent={'center'}
         alignItems={'center'}
         flexDir={'column'}
-        // mr={'2vw'}
-        // border={'1px solid green'}
+      // mr={'2vw'}
+      // border={'1px solid green'}
       >
         <motion.div
-          initial={{ opacity: 0, x: 100 }}
-          whileInView={{ x: 0, opacity: 1, transition: { duration: .2, delay: .3 } }}
+          initial={{ opacity: 0,x: 100 }}
+          whileInView={{ x: 0,opacity: 1,transition: { duration: .2,delay: .3 } }}
         // viewport={{once:true}}
         // onViewportLeave={{ opacity: 0, x: -100 ,transition: { duration: .5,delay:.5 } }}
         >
 
 
-          <Text  fontSize={['4xl', '6xl']} fontWeight={'light'} textAlign={'center'} color='red'>
-           Work Experience & Education  </Text>
+          <Text fontSize={['4xl','6xl']} fontWeight={'light'} textAlign={'center'} color='red'>
+            Work Experience & Education  </Text>
 
 
         </motion.div>
         <Box h={['50vh','100vh']} overflow={
           'scroll'
         }
-        scrollBehavior={'smooth'}
+          scrollBehavior={'smooth'}
         // border={'1px solid green'}
         >
-        <EducationTimeline items={timelineData} />
+          <EducationTimeline items={timelineData} />
         </Box>
       </Flex>
     </Flex >
