b96e595767ec0f92d3b4ea1c2350bbb050841a41
Kushal Das <kushaldas30102002@gmail.com>
2023-08-26 22:21:43 +0530

    changed timeLineData

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,9 +5,61 @@ import { ScrollText } from '../../utils/ScrollText';
 
 import EducationTimeline from './EducationTimeline'
 
-import timelineData from '../../data/timelineData.js'
+// import timelineData from '../../data/timelineData.js'
 
 export default function About() {
+
+
+
+
+  const timelineData = [
+    {
+      id: 1,
+      title: "Intern Analyst",
+      place: 'Wells Fargo',
+      location: "Bangalore, Karnataka",
+      description:
+        "Our basic task was to help Wells Fargo increase business by doing various types of analysis on the customer data and predicting the spread on different quotes of the trader. I worked on the frontend(React.js) and the backend(flask) parts. I used Chart.js for charts and used Vite for creating the react application . For unit testing I used Vitest .",
+
+      date: "May 2023 - July 2023",
+      icon: "work",
+    },
+    {
+      id: 2,
+      title: "Bachelor of Engineering",
+      place: 'Jadavpur University',
+      location: "Kolkata, West Bengal",
+      description:
+        "Joined Jadavpur University after getting a rank of 142 in WBJEE . I am currently pursuing my Bachelors in Computer Science and Engineering. I am currently in my Final year.",
+
+      date: "December 2020 - June 2023",
+      icon: "school",
+    },
+    {
+      id: 3,
+      title: "Higher Secondary",
+      place: 'Hem Sheela Model School',
+      location: "Durgapur, West Bengal",
+      description:
+        "I completed my Higher Secondary from Hem Sheela Model School . I was in CBSE board and passed my AISSCE exam with a percentage of 96.8% . I took PCMB during my school.",
+
+      date: "April 2018 - April 2020",
+      icon: "school",
+    },
+    {
+      id: 4,
+      title: "Upto Secondary",
+      place: 'Hem Sheela Model School',
+      location: "Durgapur, West Bengal",
+      description:
+        "I completed my  Secondary from Hem Sheela Model School . I was in CBSE board and passed my AISCE exam with a percentage of 97.4% . I have been studying at this school since when I was a small kid from nursery to be precise 😅.",
+
+      date: "April 2005 - May 2018",
+      icon: "school",
+    },
+
+    // Add more timeline items here
+  ];
 
   const aboutMe = [
     "Hello there, I am Kushal , a final year CSE student at Jadavpur University",
